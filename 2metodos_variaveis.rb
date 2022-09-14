a9f2b5d7663d3ddad2cfdcfed19ce1a3d142c336
joaogdfaero <joao.dal@acad.ufsm.br>
2022-09-14 16:14:51 -0300

    continuando código classes

--- 2metodos_variaveis.rb
+++ 2metodos_variaveis.rb
@@ -1,12 +1,21 @@
-# Criamos a classe com um método
-class NomeDaClasse
-    def imprimir_ola(nome)
-        puts "Olá #{nome}"
+# # Criamos a classe com um método
+# class NomeDaClasse
+#     def imprimir_ola(nome)
+#         @nome = nome # fazendo isso, a variável nome fica disponível por todo ciclo do objeto (?)
+#         puts "Olá #{nome}"
+#     end
+# end
+
+
+# # Criamos um objeto dessa classe
+# objeto = NomeDaClasse.new
+# # Usamos um método dessa classe por meio do 
+# objeto.imprimir_ola("João")
+
+class Pessoa
+    def initialize(nome)
+        @nome = nome
     end
 end
 
-
-# Criamos um objeto dessa classe
-objeto = NomeDaClasse.new
-# Usamos um método dessa classe por meio do 
-objeto.imprimir_ola("João")
+cadu = Pessoa.new("cadu")
