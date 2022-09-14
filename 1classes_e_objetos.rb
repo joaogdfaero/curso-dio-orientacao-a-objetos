a9c8956353c555ff5bd24e69d78d085266eb6983
joaogdfaero <joao.dal@acad.ufsm.br>
2022-09-14 15:58:02 -0300

    ! para mudar o original

--- 1classes_e_objetos.rb
+++ 1classes_e_objetos.rb
@@ -10,3 +10,11 @@ p objeto.object_id
 # cria um outro objeto e mostra que não muda o object id
 outro_objeto = objeto
 p outro_objeto.object_id
+
+# usa a interrogação no final do método para mudar o objeto original
+nome = "joão"
+p nome.object_id
+
+nome.upcase!
+p nome
+p nome.object_id # mesmo objeto de "joão"
