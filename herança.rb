85b1683d7c5264587f73a53d27f50cedc5c6cc3d
joaogdfaero <joao.dal@acad.ufsm.br>
2022-09-16 07:05:00 -0300

    terminei heranças

--- herança.rb
+++ herança.rb
@@ -13,7 +13,25 @@ class Sensor # SENSOR NORMAL GENÉRICO
     end
 end
 
+# CRIANDO UM OUTRO SENSOR QUE VAI HERDAR
+class SensorTemperatura < Sensor 
+    def coletar_metricas
+        puts "Estou coletando métricas de temperatura"
+        super
+    end
+end
+
+# SENSOR ORIGINAL
+p "Sensor genérico"
 sensor = Sensor.new
 sensor.instalar
 sensor.iniciar
 sensor.coletar_metricas
+
+# SENSOR DE TEMPERATURA
+p "__________________"
+p "Sensor de temperatura"
+sensor1 = SensorTemperatura.new
+sensor1.instalar
+sensor1.iniciar
+sensor1.coletar_metricas
